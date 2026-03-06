f97aec27c2abcfc930323a345119b5d11d6a91ee
Caiden-Heinrichs <cjheinrichs@students.unwsp.edu>
2026-03-06 11:53:22 -0600

    Completed program_4.py

--- program_4.py
+++ program_4.py
@@ -1,10 +1,43 @@
-# Program #4: Coordinates
-# Write a distance function that will take two 3-dimensional coordinates (as input) 
-# and will return (as output) the distance between those points in space.  
-# The 3-dimensional coordinates must be stored as tuples.
+#Week 7, Program 4 - Coordinates
+#Caiden Heinrichs
+#03/06/26
 
-# Now write a mainline that has the user enter the two tuples.  
-# The mainline calls the distance function and stores the distance in a variable.  The mainline then displays the distance.  
-# Also include exception handling to deal with faulty input.
-# The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
-#    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+import math
+
+
+#Finds the distance between two points using the formula
+def distanceBetweenPoints(point1, point2):
+    distance = math.sqrt(((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 + (point1[2] - point2[2])**2))
+    return round(distance, 4)
+
+
+def main():
+    #Collects user input for coordinates
+    try:
+        coordinateX = float(input("Enter the x coordinate for the first point: "))
+        coordinateY = float(input("Enter the y coordinate for the first point: "))
+        coordinateZ = float(input("Enter the z coordinate for the first point: "))
+    except:
+        print("Please enter valid input.")
+
+    #Builds the 1st point from the coordinates
+    point1 = (coordinateX, coordinateY, coordinateZ)
+
+    #Reassigns coordinates to new input
+    try:
+        coordinateX = float(input("Enter the x coordinate for the second point: "))
+        coordinateY = float(input("Enter the y coordinate for the second point: "))
+        coordinateZ = float(input("Enter the z coordinate for the second point: "))
+    except:
+        print("Please enter valid input.")
+
+    #Builds the 2nd point from new coordinates
+    point2 = (coordinateX, coordinateY, coordinateZ)
+
+    #Calculates the distance and prints it
+    distance = distanceBetweenPoints(point1, point2)
+    print(f'The distance between the two points is about {distance}')
+
+
+if __name__ == '__main__':
+    main()
